12dc4c2cb0f7bf9f3f7df7f692e5c5aa73e5608d
Maria B <143831892+marb543@users.noreply.github.com>
2023-09-10 23:38:07 -0400

    Set the background color and draw body of the alien

--- experiments/draw-an-alien/js/script.js
+++ experiments/draw-an-alien/js/script.js
@@ -20,7 +20,17 @@ function preload() {
 //
 // Draws a face on the canvas.
 function setup() {
-
+    // Set the canvas size
+    createCanvas(640,480);
+    //Set canvas color to pink
+    background(255,182,193);
+    //Set of code to start drawing the alien body
+    //Remove the storke color
+    noStroke();
+    //Set the color of the background to grey
+    fill(211,211,211);
+    //Draw an ellipse at the half-off bottom of the canvas
+    ellipse(320,480,400,200);
 }
 
 
