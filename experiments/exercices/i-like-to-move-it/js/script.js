7fe8b2bd2dc86fc558e9d6dfeec814ba2ecf2512
Maria B <143831892+marb543@users.noreply.github.com>
2023-09-12 15:46:32 -0400

    Start creating shapes and background

--- experiments/exercices/i-like-to-move-it/js/script.js
+++ experiments/exercices/i-like-to-move-it/js/script.js
@@ -13,12 +13,52 @@ function preload() {
 
 }
 
+//Create background object
+let bg = {
+    red:0,
+    green:255,
+    blue:255
+}
+//Create rectange1 object
+let rectangle1 = {
+    x : 0,
+    y: 500/2,
+    size : 100,
+    fill : 255 ,
+    alpha : 200,
+    speed : 1 ,
+    growthRatio : 1
+
+}
+//Create rectangle2 object
+let rectangle2 = {
+    x : 0,
+    y: 500/2,
+    size : 100,
+    fill : 255 ,
+    alpha : 200 ,
+    speed : -1 ,
+    growthRatio : 1
+
+}
+//Create triangle1 object
+let triangle1 = {
+    x : 0,
+    y: 500/2,
+    size : 100,
+    fill : 255 ,
+    alpha : 200,
+    speed:1 ,
+    growthRatio : 1
+
+}
 
 // setup()
 //
 // Sets up the canvas and the default color
 function setup() {
-
+    createCanvas(500,500);
+    
 }
 
 
@@ -26,5 +66,6 @@ function setup() {
 //
 // Creates three shapes, and animates them
 function draw() {
+    background(bg.red,bg.green,bg.blue);
 
 }
