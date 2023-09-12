a6286e7d5346ff36ef41efaac396cdbb10146d73
Maria B <143831892+marb543@users.noreply.github.com>
2023-09-12 14:41:30 -0400

    A3: Started the moving pictures activity”

---
+++ experiments/activities/03-moving-pictures/js/script.js
@@ -0,0 +1,36 @@
+/**
+ * Activity 3 : Moving pictures
+ * Maria Barba
+ * 
+ */
+
+"use strict";
+
+/**
+ * preload()
+ * 
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * setup()
+ * 
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * draw()
+ * 
+ * Description of draw()
+*/
+function draw() {
+
+}
--- experiments/workflow/README.md
+++
@@ -1,3 +0,0 @@
-# Experimenting with worflow
-
-Checking out VS Code using version control to write my programs. I feel cool right now.
