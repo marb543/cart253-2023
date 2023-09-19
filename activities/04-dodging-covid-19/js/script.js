fc7ef9aba55ff5e39080cdedad8770d5eccc0b52
Maria B <143831892+marb543@users.noreply.github.com>
2023-09-19 16:31:20 -0400

    A4 : Start dodging covid activity

---
+++ activities/04-dodging-covid-19/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Activity 4 : Dodging covid
+ * Maria Barba
+ * 
+ */
+
+"use strict";
+
+
+/**
+ * setup()
+ * 
+ * Setup canvas and set no stroke
+*/
+function setup() {
+
+}
+
+
+/**
+ * draw()
+ * 
+ * 
+*/
+function draw() {
+
+}
