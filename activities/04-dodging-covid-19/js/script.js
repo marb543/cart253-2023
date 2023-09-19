7c51a001c8b544e1fdafaf20fed266b0f28c1b81
Maria B <143831892+marb543@users.noreply.github.com>
2023-09-19 17:38:12 -0400

    A4 : Finish activity

--- activities/04-dodging-covid-19/js/script.js
+++ activities/04-dodging-covid-19/js/script.js
@@ -2,10 +2,31 @@
  * Activity 4 : Dodging covid
  * Maria Barba
  * 
+ * 
  */
 
 "use strict";
 
+let covid19 = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+    fill: {
+        r: 255,
+        g: 0,
+        b: 0
+    }
+}
+
+let user = {
+    x: 250,
+    y: 250,
+    size: 100,
+    fill: 255
+}
 
 /**
  * setup()
@@ -13,7 +34,9 @@
  * Setup canvas and set no stroke
 */
 function setup() {
-
+    createCanvas(windowWidth, windowHeight);
+    covid19.y = random(0, height);
+    covid19.vx = covid19.speed;
 }
 
 
@@ -23,5 +46,41 @@ function setup() {
  * 
 */
 function draw() {
+    background(0);
+    //Display static
+    for (let i = 0; i < 1000; i++) {
+        let x = random(0, width);
+        let y = random(0, height);
+        stroke(255);
+        point(x, y);
+    }
+
+
+
+    covid19.x = covid19.x + covid19.vx;
+    covid19.y = covid19.y + covid19.vy;
+
+    if (covid19.x > width) {
+        covid19.x = 0;
+        covid19.y = random(0, height);
+    }
+    //User movement
+    user.x = mouseX;
+    user.y = mouseY;
+
+    //Display user
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+
+    //Check for covid19
+    let d = dist(user.x, user.y, covid19.x, covid19.y);
+    if (d < covid19.size / 2 + user.size / 2) {
+        noLoop();
+    }
+
+    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+    ellipse(covid19.x, covid19.y, covid19.size);
+
+
 
 }
