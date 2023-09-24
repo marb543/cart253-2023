704d74fab890e549a7823faaaf3fa2bb3a9eb242
Maria B <143831892+marb543@users.noreply.github.com>
2023-09-24 17:07:39 -0400

    E2 : Add cat and mouse images and make them move

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -11,27 +11,37 @@
 
 //Generates a variable which will be the image of the room
 let roomImg;
+//Generates chasing cat image
+let chasingCat;
+//Generates relaxed cat image
+let relaxedCat
+//Generates mouse image
+let aliveMouse;
+//Generates a tombstone image representative of a dead mouse
+let deadMouse;
 
-let covid19 = {
+let cat = {
     x: 0,
     y: 250,
+    w: 250,
+    h: 150,
+    vx: 0,
+    vy: 0,
+    speed: 5,
+}
+
+let mouse = {
+    x: 0,
+    y: 250,
+    w: 200,
+    h: 100,
     size: 100,
     vx: 0,
     vy: 0,
     speed: 5,
-    fill: {
-        r: 255,
-        g: 0,
-        b: 0
-    }
+
 }
 
-let user = {
-    x: 250,
-    y: 250,
-    size: 100,
-    fill: 255
-}
 /**
  * preload()
  * 
@@ -39,6 +49,12 @@ let user = {
  * an image of a room, an image of a cat ,and the image of a mouse.
 */
 function preload() {
+
+    roomImg = loadImage('./assets/images/room.jpeg');
+    chasingCat = loadImage('./assets/images/runningCat.jpeg');
+    relaxedCat = loadImage('./assets/images/relaxedCat.png');
+    aliveMouse = loadImage('./assets/images/mouse.png');
+    deadMouse = loadImage('./assets/images/tombstone.png');
 
 }
 /**
@@ -48,8 +64,8 @@ function preload() {
 */
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    covid19.y = random(0, height);
-    covid19.vx = covid19.speed;
+    mouse.y = random(0, height);
+    mouse.vx = mouse.speed;
 }
 
 
@@ -59,40 +75,46 @@ function setup() {
  * 
 */
 function draw() {
-    background(0);
-    //Display static
-    for (let i = 0; i < 1000; i++) {
-        let x = random(0, width);
-        let y = random(0, height);
-        stroke(255);
-        point(x, y);
+    //Display room image
+    push();
+    image(roomImg, 0, 0, windowWidth, windowHeight);
+    pop();
+
+    //Display cat image
+    push();
+    imageMode(CENTER);
+    image(relaxedCat, cat.x, cat.y, cat.w, cat.h);
+    pop();
+
+    mouse.x = mouse.x + mouse.vx;
+    mouse.y = mouse.y + mouse.vy;
+
+    //Display mouse image
+    push();
+    imageMode(CENTER);
+    image(aliveMouse, mouse.x, mouse.y, cat.w, cat.h);
+    pop();
+
+    if (mouse.x > width) {
+        mouse.x = 0;
+        mouse.y = random(0, height);
     }
+    //Cat movement
+    // user.x = mouseX;
+    // user.y = mouseY;
 
+    // //Display user
+    // fill(user.fill);
+    // ellipse(user.x, user.y, user.size);
 
+    // //Check for covid19
+    // let d = dist(user.x, user.y, covid19.x, covid19.y);
+    // if (d < covid19.size / 2 + user.size / 2) {
+    //     noLoop();
+    // }
 
-    covid19.x = covid19.x + covid19.vx;
-    covid19.y = covid19.y + covid19.vy;
-
-    if (covid19.x > width) {
-        covid19.x = 0;
-        covid19.y = random(0, height);
-    }
-    //User movement
-    user.x = mouseX;
-    user.y = mouseY;
-
-    //Display user
-    fill(user.fill);
-    ellipse(user.x, user.y, user.size);
-
-    //Check for covid19
-    let d = dist(user.x, user.y, covid19.x, covid19.y);
-    if (d < covid19.size / 2 + user.size / 2) {
-        noLoop();
-    }
-
-    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
-    ellipse(covid19.x, covid19.y, covid19.size);
+    // fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+    // ellipse(covid19.x, covid19.y, covid19.size);
 
 
 
