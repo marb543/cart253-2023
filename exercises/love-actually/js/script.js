dd7a1cc6d9398658b9e51438d6c618249fb34519
Maria B <143831892+marb543@users.noreply.github.com>
2023-09-27 10:27:34 -0400

    E3 : Add fonts, images , and preload function

--- exercises/love-actually/js/script.js
+++ exercises/love-actually/js/script.js
@@ -3,28 +3,50 @@
  * Maria Barba
  * 
  * 
+ * All images are from Adobe Stock :
+ * Ball image : https://stock.adobe.com/ca/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bfetch_excluded_assets%5D=1&filters%5Binclude_stock_enterprise%5D=1&filters%5Bcontent_type%3Aimage%5D=1&k=ball+png&order=relevance&safe_search=1&limit=100&search_page=1&search_type=usertyped&acp=&aco=ball+png&get_facets=0&asset_id=623949235
+ * Dog image : https://stock.adobe.com/ca/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bfetch_excluded_assets%5D=1&filters%5Binclude_stock_enterprise%5D=1&filters%5Bcontent_type%3Aimage%5D=1&k=dog+png&order=relevance&safe_search=1&limit=100&search_page=1&search_type=usertyped&acp=&aco=dog+png&get_facets=0&asset_id=622226646
+ *
+ * The font used for this exercise is from Google fonts :
+ * https://fonts.google.com/specimen/Bubblegum+Sans?query=bubb
  */
 
 "use strict";
-let circle1 = {
+
+let bubblegumFont;
+
+let dog = {
     x: undefined,
     y: 250,
     size: 100,
     vx: 0,
     vy: 0,
-    speed: 3
+    speed: 3,
+    imageLink: "./assets/images/dog.png",
+    image: null
 }
-let circle2 = {
+let ball = {
     x: undefined,
     y: 250,
     size: 100,
     vx: 0,
     vy: 0,
-    speed: 3
+    speed: 3,
+    imageLink: "./assets/images/ball.png",
+    image: null
 }
 
 let state = `title`; // Can be title,love,simulation , love , sadness
-
+/**
+ * preload()
+ * 
+ * This function preloads the images
+*/
+function preload() {
+    dog.image = loadImage(dog.imageLink);
+    ball.image = loadImage(ball.imageLink);
+    bubblegumFont = loadFont(`./assets/fonts/BubblegumSans-Regular.ttf`);
+}
 /**
  * setup()
  * 
@@ -52,18 +74,20 @@ function draw() {
     }
     else if (state == `sadness`) {
         sadness();
+    } else if (state == `unrequirtedLove`) {
+
     }
 
 }
 function move() {
     //Move circles
-    circle1.x = circle1.x + circle1.vx;
-    circle1.y = circle1.y + circle1.vy;
-    circle2.x = circle2.x + circle2.vx;
-    circle2.y = circle2.y + circle2.vy;
+    dog.x = dog.x + dog.vx;
+    dog.y = dog.y + dog.vy;
+    ball.x = ball.x + ball.vx;
+    ball.y = ball.y + ball.vy;
 }
 function checkOffscreen() {
-    if (isOffscreen(circle1) || isOffscreen(circle2)) {
+    if (isOffscreen(dog) || isOffscreen(ball)) {
         state = `sadness`;
     }
 }
@@ -77,26 +101,26 @@ function isOffscreen(circle) {
 }
 function checkOverlap() {
     //Check if the circles overlap 
-    let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
-    if (d < circle1.size / 2 + circle2.size / 2) {
+    let d = dist(dog.x, dog.y, ball.x, ball.y);
+    if (d < dog.size / 2 + ball.size / 2) {
         state = `love`;
     }
 }
 function setupCircles() {
     //Position circles separated from one another
-    circle1.x = width / 3;
-    circle2.x = 2 * width / 3;
+    dog.x = width / 3;
+    ball.x = 2 * width / 3;
     //Circles velocity
-    circle1.vx = random(-circle1.speed, circle1.speed);
-    circle1.vy = random(-circle1.speed, circle1.speed);
-    circle2.vx = random(-circle1.speed, circle2.speed);
-    circle2.vy = random(-circle1.speed, circle2.speed);
+    dog.vx = random(-dog.speed, dog.speed);
+    dog.vy = random(-dog.speed, dog.speed);
+    ball.vx = random(-dog.speed, ball.speed);
+    ball.vy = random(-dog.speed, ball.speed);
 }
 
 function display() {
     //Display circles
-    ellipse(circle1.x, circle1.y, circle1.size);
-    ellipse(circle2.x, circle2.y, circle2.size);
+    ellipse(dog.x, dog.y, dog.size);
+    ellipse(ball.x, ball.y, ball.size);
 }
 function simulation() {
     move();
@@ -108,6 +132,7 @@ function love() {
     push();
     textSize(64);
     fill(255, 150, 150);
+    textFont(bubblegumFont);
     textAlign(CENTER, CENTER);
     text(`LOVE!`, width / 2, height / 2);
     pop();
@@ -116,6 +141,7 @@ function sadness() {
     push();
     textSize(64);
     fill(150, 150, 255);
+    textFont(bubblegumFont);
     textAlign(CENTER, CENTER);
     text(`D:`, width / 2, height / 2);
     pop();
@@ -124,6 +150,7 @@ function title() {
     push();
     textSize(64);
     fill(200, 100, 100);
+    textFont(bubblegumFont);
     textAlign(CENTER, CENTER);
     text("LOVE ? ", width / 2, height / 2);
     pop();
