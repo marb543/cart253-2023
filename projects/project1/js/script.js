d4aa18cee62e21437257071c76ef994c23ff2f3c
Maria B <143831892+marb543@users.noreply.github.com>
2023-10-03 14:19:35 -0400

    P1: Started project 1

---
+++ projects/project1/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Project 1
+ * 
+ * Maria Barba
+ */
+
+"use strict";
+/**
+ * preload()
+ * 
+ *This function preloads the images , and the font used 
+*/
+function preload() {
+
+}
+/**
+ * setup()
+ * 
+ * Setup canvas and tcalls the setupObjects function
+*/
+function setup() {
+
+}
+
+/**
+ * draw()
+ * 
+*/
+function draw() {
+}
