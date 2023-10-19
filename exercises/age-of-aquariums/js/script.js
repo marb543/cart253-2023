17edc0f58bf01944ea19c0d599d016cc237b90c8
Maria B <143831892+marb543@users.noreply.github.com>
2023-10-19 14:26:51 -0400

    Ex: Started Age of Aquariums exercise

---
+++ exercises/age-of-aquariums/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Exercise: The Age of Aquariums
+ * Maria Barba
+ * 
+ */
+
+"use strict";
+
+// setup()
+//
+// Does nothing.
+function preload() {
+
+}
+
+//Create background object
+
+// setup()
+//
+// Sets up the canvas and the default color
+function setup() {
+
+}
+
+
+// draw()
+//
+// Creates three shapes, and animates them
+function draw() {
+    //Set background color
+
+}
