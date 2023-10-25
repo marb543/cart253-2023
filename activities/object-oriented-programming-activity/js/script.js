db0a002a3c2776c77a2bd93b54de671c9b16c468
Maria B <143831892+marb543@users.noreply.github.com>
2023-10-25 18:26:50 -0400

    Activity: Started the Object-Oriented Programming activity

---
+++ activities/object-oriented-programming-activity/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Activity : Object Oriented Programming
+ * Maria Barba
+ * 
+ * 
+ */
+
+"use strict";
+
+/**
+ * setup()
+ * 
+ * Setup canvas and set no stroke
+*/
+function setup() {
+}
+
+/**
+ * draw()
+ * 
+ * 
+*/
+function draw() {
+
+}
+function move() {
+}
