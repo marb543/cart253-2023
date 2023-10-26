aa914a9fba6b5da883d7ec62a49cb358e59bbe6a
Maria B <143831892+marb543@users.noreply.github.com>
2023-10-26 15:23:31 -0400

    Activity : Bring back files from object oriented programming

--- activities/object-oriented-programming-activity/js/Paddle.js
+++ activities/object-oriented-programming-activity/js/Paddle.js
@@ -7,9 +7,9 @@ class Paddle {
         this.y = height - this.height / 2;
     }
 
-    // move() {
-    //     this.x = mouseX;
-    // }
+    move() {
+        this.x = mouseX;
+    }
 
     display() {
         push();
@@ -18,17 +18,6 @@ class Paddle {
         rectMode(CENTER);
         rect(this.x, this.y, this.width, this.height);
         pop();
-    }
-
-    keyPressed() {
-        console.log("Pressed key 2");
-        if (keyCode === LEFT_ARROW) {
-            this.x = this.x - 1;
-            console.log("Pressed key left");
-        } else if (keyCode === RIGHT_ARROW) {
-            this.x = this.x + 1;
-            console.log("Pressed key right");
-        }
     }
 
 }
