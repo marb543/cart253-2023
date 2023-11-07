c0d3d1f036349839d92938e81431d0562707bfc4
Maria B <143831892+marb543@users.noreply.github.com>
2023-11-07 14:43:12 -0500

    P2 : Prototype add function to display tree controls

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -23,6 +23,37 @@ let room;
 let mouseCoord;
 //Create a variable which will keep track of the state for which the user is currently is in
 let state = `title`;
+//Create an array which will store the array of objects, and will be filled with images at the preload function
+let treeDecorations = [];
+//Create a variable which stores a Christmas Tree variable
+let christmasTree = {
+    x: 340,
+    y: 350,
+    width: 180,
+    height: 150,
+    img: null
+}
+//Variable that keeps track of the position of the controls to decorate the Cristmas tree
+let ChristmasTreeChoices = {
+    star: {
+        x: 320,
+        y: 520,
+        w: 150,
+        h: 40
+    },
+    toys: {
+        x: 720,
+        y: 520,
+        w: 150,
+        h: 40
+    },
+    gift: {
+        x: 520,
+        y: 520,
+        w: 150,
+        h: 40
+    }
+}
 /**
  * preload()
  * 
@@ -30,6 +61,8 @@ let state = `title`;
 */
 function preload() {
     room = loadImage(`./assets/images/background.jpeg`);
+    christmasTree.image = loadImage(`./assets/images/ChristmasTree.png`);
+
     bubblegumFont = loadFont(`./assets/fonts/BubblegumSans-Regular.ttf`);
 }
 /**
@@ -40,7 +73,6 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
-
 /**
  * draw()
  * 
@@ -80,12 +112,82 @@ function isOffscreen(obj) {
  * This function calls the methods that are responsible for making the simulation run
  */
 function simulation() {
-    moveDog();
-    moveBall();
-    checkOffscreen();
-    checkOverlap();
-    display();
+    //Display tree 
+    displayTree();
+    //Display controls
+    displayControls();
+    //Display Timer
+    displayTimer();
 }
+/**
+ *  displayTree()
+ * 
+ * This function displays the Christmas tree
+ */
+function displayTree() {
+    //image(christmasTree.img, christmasTree.x, christmasTree.y, 100, 200);
+}
+/**
+ *  displayControls()
+ * 
+ * This function displays the controls to decorate the Christmas tree
+ */
+function displayControls() {
+    push();
+
+    drawingContext.shadowOffsetX = 2;
+    drawingContext.shadowOffsetY = -2;
+    drawingContext.shadowBlur = 8;
+    drawingContext.shadowColor = 'black';
+    textSize(25);
+    strokeWeight(4);
+    noStroke();
+    //Create rectangle behind the text
+    fill(0, 153, 255);
+    // Draw a rectangle 
+    rect(50, 50, width - 80, 100, 10);
+    //Set color
+    stroke(0);
+    fill(255, 255, 255);
+    //Set text font using the saved font
+    textFont(bubblegumFont);
+    textAlign(CENTER, CENTER);
+    text(`Select the choice of toys you want to decorate the tree with !  \n  (Hold down key 1 , 2 or 3 on your keyboard )`, width / 2, 100,);
+    pop();
+    //Draw the rectangle options for gardening
+    push();
+    drawingContext.shadowOffsetX = 2;
+    drawingContext.shadowOffsetY = -2;
+    drawingContext.shadowBlur = 8;
+    drawingContext.shadowColor = 'black';
+    fill(204, 102, 255);
+    rect(ChristmasTreeChoices.star.x, ChristmasTreeChoices.star.y, ChristmasTreeChoices.star.w, ChristmasTreeChoices.star.h, 30);
+    fill(204, 0, 0);
+    rect(ChristmasTreeChoices.toys.x, ChristmasTreeChoices.toys.y, ChristmasTreeChoices.toys.w, ChristmasTreeChoices.toys.h, 30);
+    fill(51, 204, 204);
+    rect(ChristmasTreeChoices.gift.x, ChristmasTreeChoices.gift.y, ChristmasTreeChoices.gift.w, ChristmasTreeChoices.gift.h, 30);
+    pop();
+    //Draw the text options for gardening
+    push();
+    textSize(20);
+    strokeWeight(4);
+    stroke(0);
+    fill(255);
+    textFont(bubblegumFont);
+    text(`1. Star `, width / 2 - 250, 550);
+    text(`2. Tree Toys `, width / 2 - 60, 550);
+    text(`3. Gifts `, width / 2 + 150, 550);
+    pop();
+}
+/**
+ *  displayTimer()
+ * 
+ * This function displays the timer that runs during the simulation
+ */
+function displayTimer() {
+
+}
+
 /**
  * title()
  * 
