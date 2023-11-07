7de30305ecff1bde5d6dd95ac90aac24037f7fdd
Maria B <143831892+marb543@users.noreply.github.com>
2023-11-07 15:35:31 -0500

    P2 : Project prototype display left clock

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -12,6 +12,7 @@
  * Christmas Tree no Toys : https://stock.adobe.com/ca_fr/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bfetch_excluded_assets%5D=1&filters%5Binclude_stock_enterprise%5D=1&filters%5Bcontent_type%3Aimage%5D=1&order=relevance&safe_search=1&k=Christmas+tree+cartoon&limit=100&search_page=1&search_type=usertyped&acp=&aco=Christmas+tree+cartoon&get_facets=0&asset_id=641717512
  * Christmas Tree Toys/Presents : https://stock.adobe.com/ca_fr/search?filters%5Bcontent_type%3Aphoto%5D=1&filters%5Bcontent_type%3Aillustration%5D=1&filters%5Bcontent_type%3Azip_vector%5D=1&filters%5Bcontent_type%3Avideo%5D=1&filters%5Bcontent_type%3Atemplate%5D=1&filters%5Bcontent_type%3A3d%5D=1&filters%5Bfetch_excluded_assets%5D=1&filters%5Binclude_stock_enterprise%5D=1&filters%5Bcontent_type%3Aimage%5D=1&order=relevance&safe_search=1&k=Christmas+tree+decorations+cartoon&limit=100&search_page=1&search_type=usertyped&acp=&aco=Christmas+tree+decorations+cartoon&get_facets=0&asset_id=379224188
  * 
+ * I modified the code example from this page where I created a clock in JavaScript: https://p5js.org/examples/input-clock.html
  */
 
 "use strict";
@@ -25,7 +26,7 @@ let mouseCoord;
 let state = `title`;
 //Create an array which will store the array of objects, and will be filled with images at the preload function
 let treeDecorations = [];
-//Create a variable to store the values of the clock
+//Create variables to store the values of the clock
 let cx, cy;
 let secondsRadius;
 let minutesRadius;
@@ -68,7 +69,6 @@ let ChristmasTreeChoices = {
 function preload() {
     room = loadImage(`./assets/images/background.jpeg`);
     christmasTree.img = loadImage(`./assets/images/ChristmasTree.png`);
-
     bubblegumFont = loadFont(`./assets/fonts/BubblegumSans-Regular.ttf`);
 }
 /**
@@ -78,6 +78,7 @@ function preload() {
 */
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    setupClock();
 }
 /**
  * draw()
@@ -154,7 +155,7 @@ function displayControls() {
     strokeWeight(4);
     noStroke();
     //Create rectangle behind the text
-    fill(0, 153, 255);
+    fill(71, 44, 175);
     // Draw a rectangle 
     rect(50, 50, 300, 400, 10);
     //Set color
@@ -196,9 +197,59 @@ function displayControls() {
  * This function displays the timer that runs during the simulation
  */
 function displayTimer() {
+    push();
+    //Add shadows to shapes 
+    drawingContext.shadowOffsetX = 2;
+    drawingContext.shadowOffsetY = -2;
+    drawingContext.shadowBlur = 8;
+    drawingContext.shadowColor = 'black';
+    // Draw the clock background
+    noStroke();
+    fill(255);
+    ellipse(cx, cy, clockDiameter + 25, clockDiameter + 25);
+    fill(113, 53, 113);
+    ellipse(cx, cy, clockDiameter, clockDiameter);
 
+    // Angles for sin() and cos() start at 3 o'clock;
+    // subtract HALF_PI to make them start at the top
+    let s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
+    let m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
+    let h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
+
+    // Draw the hands of the clock
+    stroke(255);
+    strokeWeight(1);
+    line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
+    strokeWeight(2);
+    line(cx, cy, cx + cos(m) * minutesRadius, cy + sin(m) * minutesRadius);
+    strokeWeight(4);
+    line(cx, cy, cx + cos(h) * hoursRadius, cy + sin(h) * hoursRadius);
+    // Draw the minute ticks
+    strokeWeight(2);
+    beginShape(POINTS);
+    for (let a = 0; a < 360; a += 6) {
+        let angle = radians(a);
+        let x = cx + cos(angle) * secondsRadius;
+        let y = cy + sin(angle) * secondsRadius;
+        vertex(x, y);
+    }
+    endShape();
+    pop();
 }
-
+/**
+ * setupClock()
+ * 
+ * This method sets up all the variables necessary to making a clock work
+ */
+function setupClock() {
+    let radius = 110;
+    secondsRadius = radius * 0.71;
+    minutesRadius = radius * 0.6;
+    hoursRadius = radius * 0.5;
+    clockDiameter = radius * 1.7;
+    cx = 200;
+    cy = 300;
+}
 
 /**
  * title()
