473fe5e87a8409b0a3d253455c65ead40587598b
Maria B <143831892+marb543@users.noreply.github.com>
2023-11-07 14:07:08 -0500

    P2 : Add missing image to prototype, cleanup the code

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -18,37 +18,10 @@
 //Create a variable to store the font
 let bubblegumFont;
 //Create a variable to store the background image
-let parkImage;
+let room;
 //Create a variable to keep track of the mouse coordinates
 let mouseCoord;
-//Create a dog object
-let dog = {
-    x: undefined,
-    y: 250,
-    width: 130,
-    height: 200,
-    vx: 0,
-    vy: 0,
-    speed: 3,
-    imageLink: "./assets/images/dog.png",
-    image: null
-}
-//Create a ball object
-let ball = {
-    x: undefined,
-    y: 250,
-    width: 100,
-    height: 200,
-    vx: 0,
-    vy: 0,
-    speed: 3,
-    imageLink: "./assets/images/ball.png",
-    image: null,
-    tx: 0,
-    ty: 10,
-}
 //Create a variable which will keep track of the state for which the user is currently is in
-
 let state = `title`;
 /**
  * preload()
@@ -56,9 +29,7 @@ let state = `title`;
  *This function preloads the images , and the font used 
 */
 function preload() {
-    dog.image = loadImage(dog.imageLink);
-    ball.image = loadImage(ball.imageLink);
-    parkImage = loadImage(`./assets/images/park.jpeg`);
+    room = loadImage(`./assets/images/background.jpeg`);
     bubblegumFont = loadFont(`./assets/fonts/BubblegumSans-Regular.ttf`);
 }
 /**
@@ -67,8 +38,7 @@ function preload() {
  * Setup canvas and tcalls the setupObjects function
 */
 function setup() {
-    createCanvas(800, 800);
-    setupObjects();
+    createCanvas(windowWidth, windowHeight);
 }
 
 /**
@@ -78,71 +48,17 @@ function setup() {
  * certain state has been reached, the according function is called.
 */
 function draw() {
-    background(parkImage);
+    background(room);
     if (state == `title`) {
         title();
     } else if (state == `simulation`) {
         simulation();
     }
-    else if (state == `happiness`) {
-        happyDog();
+    else if (state == `gameOver`) {
+        gameOver();
     }
-    else if (state == `sadness`) {
-        sadDog();
-    } else if (state == `superHappy`) {
-        extremelyHappyDog();
-    }
-}
-/**
- * moveDog()
- * 
- * This function moves the dog object using keyboard key controls A, W , S , D
- */
-function moveDog() {
-    if (keyIsPressed) {
-        switch (key) {
-            case 'a' || 'A':
-                dog.x = dog.x - 1 * dog.vx;
-                break;
-            case 'd' || 'D':
-                dog.x = dog.x + 1 * dog.vx;
-                break;
-            case 'w' || 'W':
-                dog.y = dog.y - 1 * dog.vy;
-                break;
-            case 's' || 'S':
-                dog.y = dog.y + 1 * dog.vy;
-                break;
-        }
-    }
-}
-/**
- * moveBall()
- * 
- * The moveBall function makes the ball move according to noise movement.
- */
-function moveBall() {
-    //Set tx,ty
-    ball.tx = ball.tx + 0.025;
-    ball.ty = ball.ty + 0.025;
-    //Create and set noiseX, noiseY variables
-    let noiseX = noise(ball.tx);
-    let noiseY = noise(ball.ty);
-    //Set the velocity x , velocity y
-    ball.vx = map(noiseX, 0, 1, -ball.speed, ball.speed);
-    ball.vy = map(noiseY, 0, 1, -ball.speed, ball.speed);
-    //Set x,y coordinates
-    ball.x = ball.x + ball.vx;
-    ball.y = ball.y + ball.vy;
-}
-/**
- * checkOffscreen()
- * 
- * This function checks if either the dog or the ball are offscreen , and if they are then set the state accordingly
- */
-function checkOffscreen() {
-    if (isOffscreen(dog) || isOffscreen(ball)) {
-        state = `sadness`;
+    else if (state == `gameWon`) {
+        gameWon();
     }
 }
 /**
@@ -159,45 +75,6 @@ function isOffscreen(obj) {
     }
 }
 /**
- * checkOverlap()
- * 
- * This function verifies if the dog and the ball object are on top of eachother, and if they are it sets the state accordingly
- */
-function checkOverlap() {
-    //Check if the circles overlap 
-    let d = dist(dog.x, dog.y, ball.x, ball.y);
-    if (d < (dog.width + ball.width) / 2) {
-        state = `happiness`;
-    }
-}
-/**
- * setupObjects()
- * 
- * This function assigns random x,y positions to dog,ball , as well as velocity for both objects
- */
-function setupObjects() {
-    //Position of dog and ball are generated randomly
-    dog.x = random(0, width - 200);
-    dog.y = random(0, height - 200);
-    ball.x = random(0, width - 200);
-    ball.y = random(0, height - 200);
-    //Dog and ball velocity
-    dog.vx = dog.speed;
-    dog.vy = dog.speed;
-    ball.vx = random(-dog.speed / 2, ball.speed / 2);
-    ball.vy = random(-dog.speed / 2, ball.speed / 2);
-}
-/** 
- * display()
- * 
- * This function displays to the screen the image of the dog , and ball
-*/
-function display() {
-    //Display dog and ball
-    image(dog.image, dog.x, dog.y, dog.width, dog.height);
-    image(ball.image, ball.x, ball.y, ball.width, ball.width);
-}
-/**
  * simulation()
  * 
  * This function calls the methods that are responsible for making the simulation run
@@ -208,54 +85,6 @@ function simulation() {
     checkOffscreen();
     checkOverlap();
     display();
-}
-/**
- * happyDog()
- * 
- * This function sets the text for the happiness state
- */
-function happyDog() {
-    push();
-    textSize(50);
-    strokeWeight(4);
-    stroke(250);
-    fill(255, 150, 150);
-    textFont(bubblegumFont);
-    textAlign(CENTER, CENTER);
-    text(`Jack is a happy dog now !`, width / 2, height / 2);
-    pop();
-}
-/**
- * sadDog()
- * 
- * This function sets the text for the sadness state
- */
-function sadDog() {
-    push();
-    textSize(50);
-    strokeWeight(4);
-    stroke(250);
-    fill(150, 150, 255);
-    textFont(bubblegumFont);
-    textAlign(CENTER, CENTER);
-    text(`No ! Jack !`, width / 2, height / 2);
-    pop();
-}
-/**
- * extremelyHappyDog()
- * 
- * This function sets the text for the superHappy state
- */
-function extremelyHappyDog() {
-    push();
-    textSize(45);
-    strokeWeight(4);
-    stroke(250);
-    fill(252, 202, 3);
-    textFont(bubblegumFont);
-    textAlign(CENTER, CENTER);
-    text(`You caught the ball for Jack !`, width / 2, height / 2);
-    pop();
 }
 /**
  * title()
@@ -270,7 +99,7 @@ function title() {
     fill(200, 100, 100);
     textFont(bubblegumFont);
     textAlign(CENTER, CENTER);
-    text("Will Jack catch the ball ? ", width / 2, height / 2);
+    text("Decorate the Christmas Tree before the timer runs out ! ", width / 2, height / 2);
     pop();
 }
 /**
@@ -281,16 +110,5 @@ function title() {
 function mousePressed() {
     if (state == `title`) {
         state = `simulation`;
-    }
-}
-/**
- * mouseClicked()
- * 
- * This function checks if the user had clicked on the ball to set the superHappy state
- */
-function mouseClicked() {
-    let d = dist(mouseX, mouseY, ball.x, ball.y);
-    if (d < ball.width) {
-        state = `superHappy`;
     }
 }
